92a82b0507f4886b610ed62e31ae9c81d307c5a4
Ismael de Laet Abashi <ismaelabashi@hotmail.com>
2022-06-09 18:36:19 -0300

    Feat: Add class Squad

--- register_squads.py
+++ register_squads.py
@@ -2,3 +2,12 @@ class Person:
     def __init__(self, name, phone):
         self.name = name
         self.phone = phone
+
+class Squad:
+    def __init__(self, name, techlead = None, devs = None):
+        self.name = name
+        self.techlead = techlead
+        self.devs = []
+
+    def include_techlead(self, techlead):
+        self.techlead = techlead
